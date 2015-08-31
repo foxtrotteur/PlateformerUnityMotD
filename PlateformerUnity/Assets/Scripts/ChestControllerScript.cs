3ba26b3854c0b5bebd4edc94feef1b5795811e69
Ludovic Cossardeaux <ludovic.cossardeaux@worldline.com>
2015-08-31 17:58:44 +0200

    Commit chest

---
+++ PlateformerUnity/Assets/Scripts/ChestControllerScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChestControllerScript : MonoBehaviour {
+
+    Animator anim;
+    public bool opened = false;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (opened) { anim.Play("Chest_Opened"); }
+        //
+
+	
+	}
+
+    void OnTriggerEnter (Collider other){
+        anim.Play("Chest_Opening");
+            opened = true;
+        
+        //Destroy(other.gameObject);
+    }
+}
