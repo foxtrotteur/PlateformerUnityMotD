1338f46bef3b0e8ef52ab9d3e060d6242977d886
Nicolas Lozier <nicolas.lozier@atos.net>
2015-09-22 13:44:47 +0200

    Commit image ? loul

--- PlateformerUnity/Assets/Scripts/BlocConstant.cs
+++ PlateformerUnity/Assets/Scripts/BlocConstant.cs
@@ -5,7 +5,7 @@
     public const int EXIT_NUMBER = 3;
 
     public const string BLOC_NAME = "prefabBloc_13";
-    public const string ENTER_NAME = "exitPrefab";
+    public const string ENTER_NAME = "entryPrefab";
     public const string EXIT_NAME = "exitPrefab";
 }
 
